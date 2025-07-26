3a8594896b30c00be7dd808b37731aa39a2584ab
angelabtte <angel.bts12@gmail.com>
2025-07-26 10:04:11 -0500

    actualizacion

--- Ansible/deploy.sh
+++ Ansible/deploy.sh
@@ -5,10 +5,10 @@ echo "Iniciando despliegue con Ansible"
 # Verificar si se proporciona una contrasena de Ansible Vault
 if [ -z "$1" ]; then
   echo "No se ha proporcionado contraseña de Ansible Vault. Ejecutando sin vault..."
-  ansible-playbook -i hosts playbook.yml
+  ansible-playbook -i inventory/hosts.ini playbook.yml
 else
   echo "Usando contraseña de Ansible Vault..."
-  ansible-playbook -i hosts playbook.yml --ask-vault-pass
+  ansible-playbook -i inventory/hosts.ini playbook.yml --ask-vault-pass
 fi
 
 echo "Despliegue finalizado."
